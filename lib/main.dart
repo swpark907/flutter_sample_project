89517f43d39e3c258ef3900c08e940ae342b7322
swpark907 <swpark907@naver.com>
2023-02-02 13:40:52 +0900

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sample_project/constants/constants.dart';
 import 'package:sample_project/firebase_options.dart';
 import 'package:sample_project/views/login_view.dart';
 import 'package:sample_project/views/register_view.dart';
@@ -15,9 +16,9 @@ void main() {
     theme: ThemeData(primarySwatch: Colors.blue),
     home: const HomePage(),
     routes: {
-      '/login/': (context) => const LoginView(),
-      '/register/': (context) => const RegisterView(),
-      '/notes/': (context) => const NotesView(),
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
     },
   ));
 }
@@ -78,7 +79,7 @@ class _NotesViewState extends State<NotesView> {
                     if (shouldLogout) {
                       await FirebaseAuth.instance.signOut();
                       Navigator.of(context).pushNamedAndRemoveUntil(
-                        "/login/",
+                        loginRoute,
                         (_) => false,
                       );
                     }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import "package:firebase_auth/firebase_auth.dart";
+import 'package:sample_project/constants/constants.dart';
 import 'package:sample_project/firebase_options.dart';
 import "dart:developer" as devtools show log;
 
@@ -71,7 +72,7 @@ class _RegisterViewState extends State<RegisterView> {
           TextButton(
               onPressed: () {
                 Navigator.of(context)
-                    .pushNamedAndRemoveUntil('/login/', (route) => false);
+                    .pushNamedAndRemoveUntil(loginRoute, (route) => false);
               },
               child: const Text("Already registered? Login here!"))
         ],
