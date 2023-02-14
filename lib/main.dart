534de155536bbee467205322715300ffe9059381
swpark907 <swpark907@naver.com>
2023-02-14 20:22:02 +0900

    step-13

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sample_project/constants/constants.dart';
 import 'package:sample_project/services/auth/auth_service.dart';
 import 'package:sample_project/views/login_view.dart';
+import 'package:sample_project/views/notes/new_note_view.dart';
 import 'package:sample_project/views/notes_view.dart';
 import 'package:sample_project/views/register_view.dart';
 import 'package:sample_project/views/verify_email_view.dart';
@@ -18,6 +19,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
